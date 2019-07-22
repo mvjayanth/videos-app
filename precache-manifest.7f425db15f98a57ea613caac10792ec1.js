4400c25281b579f6600fb7d86b72933f6ce3d2d0
Matta Viswa Jayanth <m.viswajayanth@gmail.com>
2019-07-22 19:03:30 +0530

    Updates

---
+++ precache-manifest.7f425db15f98a57ea613caac10792ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c44af5cb411cbf1f6760fa682946d6",
+    "url": "/videos-app/index.html"
+  },
+  {
+    "revision": "ded3db2ef8a821edc1b7",
+    "url": "/videos-app/static/css/main.4b2fc5f7.chunk.css"
+  },
+  {
+    "revision": "e9c2ec1ac571ad625f91",
+    "url": "/videos-app/static/js/2.60637844.chunk.js"
+  },
+  {
+    "revision": "ded3db2ef8a821edc1b7",
+    "url": "/videos-app/static/js/main.64955a4b.chunk.js"
+  },
+  {
+    "revision": "fba9581061e3e64cad09",
+    "url": "/videos-app/static/js/runtime~main.2f537c80.js"
+  }
+]);
